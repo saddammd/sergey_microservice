dbfc3dd2e1755b373c86d59bf36a3592739617ea
saddammd <saddam.md@gmail.com>
2021-10-20 08:23:43 +0530

    test1

--- demo.user.microservice/src/main/java/com/microservice/practical/demo/user/microservice/controller/User_Controller.java
+++ demo.user.microservice/src/main/java/com/microservice/practical/demo/user/microservice/controller/User_Controller.java
@@ -3,6 +3,6 @@ package com.microservice.practical.demo.user.microservice.controller;
 public class User_Controller {
 
 	public void show() {
-		System.out.println("hi");
+		System.out.println("user");
 	}
 }
